a32a6065343148272c47e2a7da04a8b450750247
takacsagi95 <takacsagi95@gmail.com>
2022-12-07 13:18:15 +0100

    project init.

---
+++ src/main/java/com/example/GreenBay/GreenBayApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreenBay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GreenBay/GreenBayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenBay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
